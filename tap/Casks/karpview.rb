a5e9b51cbf4353aee6229199a24e2739db0f181f
Nik Gibson <nikkig124@gmail.com>
2026-03-31 10:21:28 -0700

    chore: update cask SHA256 for v0.1.0

--- tap/Casks/karpview.rb
+++ tap/Casks/karpview.rb
@@ -1,6 +1,6 @@
 cask "karpview" do
   version "0.1.0"
-  sha256 "REPLACE_WITH_SHA256_FROM_MAKE_RELEASE"
+  sha256 "6ab5f3c1a8df90fe79adf965bb8d8e11a8d0c913c7775749cbffb3fcef5fab05"
 
   url "https://github.com/nikgibson/karpview/releases/download/v#{version}/karpview-v#{version}-darwin-arm64.tar.gz"
   name "KarpView"
