f435dccada73a39eb288456879ae6ff04edd23ad
Nik Gibson <nikkig124@gmail.com>
2026-03-31 10:28:03 -0700

    fix: correct repo URL in cask from nikgibson to n-gibs

--- tap/Casks/karpview.rb
+++ tap/Casks/karpview.rb
@@ -2,10 +2,10 @@ cask "karpview" do
   version "0.1.0"
   sha256 "6ab5f3c1a8df90fe79adf965bb8d8e11a8d0c913c7775749cbffb3fcef5fab05"
 
-  url "https://github.com/nikgibson/karpview/releases/download/v#{version}/karpview-v#{version}-darwin-arm64.tar.gz"
+  url "https://github.com/n-gibs/karpview/releases/download/v#{version}/karpview-v#{version}-darwin-arm64.tar.gz"
   name "KarpView"
   desc "Karpenter node disruption visualizer"
-  homepage "https://github.com/nikgibson/karpview"
+  homepage "https://github.com/n-gibs/karpview"
 
   binary "karpview-darwin-arm64", target: "karpview"
 end
